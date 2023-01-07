2d71b1d539ce5a1f61ac4def53f18d35cc68dac0
Sharat Katwa <sjkatwa2000@gmail.com>
2023-01-07 11:22:18 +0530

    learned about class,public and private properties,getters and setters in typescript

--- pureTS/dist/index.js
+++ pureTS/dist/index.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log('typescript is here still');
+// class User {
+//   email: string
+//   name: string
+//   private readonly city: string = 'bangalore'
+//   constructor(name: string, email: string){
+//     this.email = email
+//     this.name = name
+//   }
+// }
+// const sharat = new User('sharat', 's@s.com')
+// // sharat.city
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this._courseCount = 1;
+        this.city = 'bangalore';
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    tokenDelete() {
+        console.log('token deleted');
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNum;
+    }
+}
+const sharat = new User('sharat', 's@s.com');
+// sharat.city
+// sharat.tokenDelete()
--- pureTS/src/index.ts
+++ pureTS/src/index.ts
@@ -1,1 +1,45 @@
-console.log('typescript is here still')
+// class User {
+//   email: string
+//   name: string
+//   private readonly city: string = 'bangalore'
+//   constructor(name: string, email: string){
+//     this.email = email
+//     this.name = name
+//   }
+// }
+
+// const sharat = new User('sharat', 's@s.com')
+// // sharat.city
+
+
+class User {
+  private _courseCount = 1
+
+  private readonly city: string = 'bangalore'
+  constructor(public name: string, public email: string){
+  }
+  get getAppleEmail(): string {
+    return `apple${this.email}`
+  }
+
+  private tokenDelete(){
+    console.log('token deleted')
+  }
+
+  get courseCount(): number {
+    return this._courseCount
+  }
+
+  set courseCount(courseNum){
+    if(courseNum <= 1){
+      throw new Error('Course count should be more than 1')
+    }
+    this._courseCount = courseNum
+  }
+
+}
+
+
+const sharat = new User('sharat', 's@s.com')
+// sharat.city
+// sharat.tokenDelete()
