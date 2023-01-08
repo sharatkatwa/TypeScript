765fe9ed0a0728f79a749cc416bf7d44d2f42e79
Sharat Katwa <sjkatwa2000@gmail.com>
2023-01-08 11:14:39 +0530

    learned about the some more narrowing in typescript

--- pureTS/dist/detection.js
+++ pureTS/dist/detection.js
@@ -5,3 +5,35 @@ function AdminAccount(account) {
     }
     return account.normalUser;
 }
+function getDateOrString(bot) {
+    if (bot instanceof Date)
+        return bot;
+    return 'this is not a date';
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return 'fish food';
+    }
+    pet;
+    return 'bird food';
+}
+function getRectangleRadius(shape) {
+    if (shape.kind === 'rectangle') {
+        return shape.length * shape.width;
+    }
+    return 'this is not a rectangle';
+}
+function getAria(shape) {
+    switch (shape.kind) {
+        case 'circle': return Math.PI * shape.radius * 2;
+        case 'square': return shape.side * shape.side;
+        case 'rectangle': return shape.length * shape.width;
+        default:
+            const _defaultShape = shape;
+            return _defaultShape;
+    }
+}
--- pureTS/src/detection.ts
+++ pureTS/src/detection.ts
@@ -16,3 +16,62 @@ interface user {
     }
     return account.normalUser
  }
+
+ function getDateOrString(bot: Date | string){
+  if(bot instanceof Date) return bot
+  return 'this is not a date'
+ }
+
+ type Fish = {swim: ()=>void}
+ type Bird = {fly: ()=>void}
+
+ function isFish(pet: Fish | Bird): pet is Fish{
+  return (pet as Fish).swim !== undefined
+ }
+
+ function getFood(pet: Fish | Bird){
+  if(isFish(pet)){
+    pet
+    return 'fish food'
+  }
+  pet
+  return 'bird food'
+ }
+
+
+ interface Circle {
+  kind: 'circle'
+  radius: number
+ }
+ interface Square {
+   kind: 'square'
+   side: number
+ }
+ interface Rectangle {
+  kind: 'rectangle'
+  length: number
+  width: number
+ }
+
+ type shape = Circle | Square | Rectangle
+
+ function getRectangleRadius(shape: shape){
+  if(shape.kind === 'rectangle'){
+    return shape.length * shape.width 
+  }
+  return 'this is not a rectangle'
+ }
+
+ function getAria(shape: shape){
+   switch(shape.kind){
+    case 'circle': return Math.PI * shape.radius * 2
+    
+    case 'square': return shape.side * shape.side
+    
+    case 'rectangle': return shape.length * shape.width
+    
+    default: const _defaultShape: never = shape
+      return _defaultShape
+  
+  }
+ }
